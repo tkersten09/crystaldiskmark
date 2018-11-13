ba1385ca23ecfb334cadbd164716ae8aa4214f6c
tkersten09 <tkersten09@gmail.com>
2018-11-13 18:22:23 +0100

    Updated scripts
    - the format of the latest package example is used for the scripts, hence the following is used:
      * packageArgs array
      *  Get-UninstallRegistryKey
    - url and silentArgs are updated
    - sha256 is now used for hash check

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,15 +1,11 @@
-﻿$packageName = '{{PackageName}}'
-$installerType = 'exe'
-$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
-$url = '{{DownloadUrl}}'
-$checksum = '{{Checksum}}'
-$checksumType = 'sha1'
-$validExitCodes = @(0)
-
-Install-ChocolateyPackage -PackageName "$packageName" `
-                          -FileType "$installerType" `
-                          -SilentArgs "$silentArgs" `
-                          -Url "$url" `
-                          -ValidExitCodes $validExitCodes `
-                          -Checksum "$checksum" `
-                          -ChecksumType "$checksumType"
+﻿$ErrorActionPreference = 'Stop';
+$packageArgs = @{
+    packageName = $env:ChocolateyPackageName
+    installerType = 'exe'
+    silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+    url = 'https://osdn.net/frs/redir.php?f=crystaldiskmark%2F68624%2FCrystalDiskMark6_0_2.exe'
+    checksum = '84ccc2dc20e33abe48e9e452566213edf36adf5f308854fa55a9e15ff9efa2e0'
+    checksumType = 'sha256'
+    validExitCodes = @(0)
+}
+Install-ChocolateyPackage @packageArgs
--- tools/chocolateyUninstall.ps1
+++ tools/chocolateyUninstall.ps1
@@ -1,14 +1,26 @@
-﻿$packageName = '{{PackageName}}'
-$softwareName = "$packageName*"
-$installerType = 'exe'
-$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
-$validExitCodes = @(0)
+﻿$ErrorActionPreference = 'Stop';
+$packageArgs = @{
+    packageName   = $env:ChocolateyPackageName
+    softwareName  = 'CrystalDiskMark*'
+    fileType      = 'exe'
+    silentArgs    = "/VERYSILENT"
+    validExitCodes= @(0)
+}
 
-[array]$key = Get-UninstallRegistryKey -SoftwareName $softwareName
-$key | ForEach-Object {
-  Uninstall-ChocolateyPackage -PackageName $packageName `
-                              -FileType $installerType `
-                              -SilentArgs $($silentArgs) `
-                              -File $($_.UninstallString.Replace('"','')) `
-                              -ValidExitCodes $validExitCodes
+$uninstalled = $false
+[array]$key = Get-UninstallRegistryKey -SoftwareName $packageArgs['softwareName']
+
+if ($key.Count -eq 1) {
+  $key | % {
+    $packageArgs['file']    = "$($_.UninstallString)"
+    
+    Uninstall-ChocolateyPackage @packageArgs
+  }
+} elseif ($key.Count -eq 0) {
+  Write-Warning "$packageName has already been uninstalled by other means."
+} elseif ($key.Count -gt 1) {
+  Write-Warning "$($key.Count) matches found!"
+  Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
+  Write-Warning "Please alert package maintainer the following keys were matched:"
+  $key | % {Write-Warning "- $($_.DisplayName)"}
 }
